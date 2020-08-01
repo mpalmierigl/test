d02508aaae45ee02937c3ada856412d9d8d7488b
mpalmierigl <martin.palmieri@globallogic.com>
2020-08-01 18:52:51 +0000

    Updates

---
+++ precache-manifest.3947a6cdb732723f613b411f74c4d548.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b54360f200443770f30663a1fa19be",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "0acbf342d9625b6420d0",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/test/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0acbf342d9625b6420d0",
+    "url": "/test/static/js/main.6a573bac.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
